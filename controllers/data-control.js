9fa8bdf949ce223d6eb9097502825cfe62b0c5a6
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-03 21:09:36 -0500

    start

---
+++ controllers/data-control.js
@@ -0,0 +1,21 @@
+import CONFIG from "../config/config.js";
+
+import { runPerplexitySearch } from "../src/src.js";
+
+export const getBackendValueControl = async (req, res) => {
+  const { key } = req.body;
+  if (!key) return null;
+
+  const value = CONFIG[key];
+
+  return res.json(value);
+};
+
+// export const searchControl = async (req, res) => {
+//   const { query } = req.body;
+
+//   const data = await runPerplexitySearch(query);
+//   if (!data) return null;
+
+//   return res.json(data);
+// };
---
+++ routes/router.js
@@ -0,0 +1,32 @@
+import express from "express";
+import requireAuth from "./auth-route.js";
+
+import { authControl } from "../controllers/auth-control.js";
+// import { getBackendValueControl, submitControl } from "../controllers/data-control.js";
+import { displayMain, display404, display500, display401 } from "../controllers/display-control.js";
+
+import CONFIG from "../config/config.js";
+// const { loginRoute, perplexityRoute } = CONFIG;
+
+const router = express.Router();
+
+router.get("/401", display401);
+
+//no auth required obv
+router.post("/site-auth-route", authControl);
+
+// router.post("/get-backend-value-route", requireAuth, getBackendValueControl);
+
+//submit
+// router.post(CONFIG.submitRoute, requireAuth, submitControl);
+
+// //perplexity
+// router.post(perplexityRoute, requireAuth, perplexityControl);
+
+router.get("/", requireAuth, displayMain);
+
+router.use(display404);
+
+router.use(display500);
+
+export default router;
