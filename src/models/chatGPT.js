1d3f4b58722e9e9b76a0898c104b8d0cb8a047f2
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-05 09:19:56 -0500

    todo

--- src/models/chatGPT.js
+++ src/models/chatGPT.js
@@ -4,15 +4,20 @@ import CONFIG from "../../config/config.js";
 
 const client = new OpenAI({
   apiKey: CONFIG.OPENAI_API_KEY,
+  //ADD BASE URL
+
 });
 
 export const runChatGPT = async (inputParams) => {
   const { prompt, model } = inputParams;
   if (!prompt || !model) return null;
 
+  console.log("CHATGPT PARAMS");
+  console.dir(inputParams);
+
   const response = await client.responses.create({
     model: model,
-    tools: [{ type: "web_search" }],
+    // tools: [{ type: "web_search" }],
     input: prompt,
   });
 
