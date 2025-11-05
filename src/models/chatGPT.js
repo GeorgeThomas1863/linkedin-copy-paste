22f5bea6fd222c2e558f3a3e4a7ba646c50fdd0d
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-04 21:27:57 -0500

    need to build

--- src/models/chatGPT.js
+++ src/models/chatGPT.js
@@ -1,1 +1,22 @@
 //BUILD
+import OpenAI from "openai";
+import CONFIG from "../../config/config.js";
+
+const client = new OpenAI({
+  apiKey: CONFIG.OPENAI_API_KEY,
+});
+
+export const runChatGPT = async (prompt) => {
+  const response = await client.responses.create({
+    model: "gpt-5-nano",
+    tools: [{ type: "web_search" }],
+    input: prompt,
+  });
+
+  console.log("CHATGPT RESPONSE");
+  console.dir(response);
+  console.log("MESSAGE RESPONSE");
+  console.log(response.output_text);
+
+  return response;
+};
